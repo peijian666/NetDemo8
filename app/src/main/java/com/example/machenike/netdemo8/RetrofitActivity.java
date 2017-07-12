fb85b10ecfc82698c95d1bec50f3b2b67db536d4
jthug <1649369473@qq.com>
2017-07-12 16:16:28 +0800

    Retrofit的Post、表单、多部分请求

--- app/src/main/java/com/example/machenike/netdemo8/RetrofitActivity.java
+++ app/src/main/java/com/example/machenike/netdemo8/RetrofitActivity.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,6 +18,12 @@ public class RetrofitActivity extends AppCompatActivity {
 
     @BindView(R.id.btn_get)
     Button mBtnGet;
+    @BindView(R.id.btn_post)
+    Button mBtnPost;
+    @BindView(R.id.btn_form)
+    Button mBtnForm;
+    @BindView(R.id.btn_part)
+    Button mBtnPart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +33,79 @@ public class RetrofitActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.btn_get)
-    public void onViewClicked() {
-        RetrofitNetClient.getInstance().getRetrofitApi().getRetrofit().enqueue(new Callback<ResponseBody>() {
+    public void get() {
+        RetrofitNetClient.getInstance().getRetrofitApi().getRetrofit("search").enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                Toast.makeText(RetrofitActivity.this,"成功了",Toast.LENGTH_SHORT).show();
+                //运行在主线程中，可以直接更新UI
+                if (response.isSuccessful()) {
+                    Toast.makeText(RetrofitActivity.this, "成功了", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RetrofitActivity.this, response.code() + "", Toast.LENGTH_SHORT).show();
+                }
+
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Toast.makeText(RetrofitActivity.this,"失败了",Toast.LENGTH_SHORT).show();
+                Toast.makeText(RetrofitActivity.this, "失败了", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @OnClick(R.id.btn_post)
+    public void post() {
+        RequestBody requestBody = RequestBody.create(null, "{\n" +
+                "\"Password\":\"654321\"\n" +
+                "\"UserName\":\"qjd\",\n" +
+                "}");
+        RetrofitNetClient.getInstance().getRetrofitApi().postRetrofit(requestBody).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                Toast.makeText(RetrofitActivity.this, "成功了" + response.code(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(RetrofitActivity.this, "失败了", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @OnClick(R.id.btn_form)
+    public void form() {
+        RetrofitNetClient.getInstance().getRetrofitApi().formRetrofit("张三", "123").enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                Toast.makeText(RetrofitActivity.this, "成功了" + response.code(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(RetrofitActivity.this, "失败", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @OnClick(R.id.btn_part)
+    public void multiPart() {
+        RequestBody requestBody = RequestBody.create(null, "{\n" +
+                "\"name\": \"yt59856b15cf394e7b84a7d48447d16098\",\n" +
+                "\"username\": \"xc62\",\n" +
+                "\"nickname\": \"555\",\n" +
+                "\"password\": \"123456\",\n" +
+                "\"uuid\": \"0F8EC12223174657B2E842076D54C361\"\n" +
+                "}");
+        RetrofitNetClient.getInstance().getRetrofitApi().multiRetrofit(requestBody).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                Toast.makeText(RetrofitActivity.this, "成功了" + response.code(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(RetrofitActivity.this, "失败", Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/example/machenike/netdemo8/RetrofitApi.java
+++ app/src/main/java/com/example/machenike/netdemo8/RetrofitApi.java
@@ -1,15 +1,39 @@
 package com.example.machenike.netdemo8;
 
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Path;
 
 /**
  * Created by MACHENIKE on 2017/7/12.
  */
 
 public interface RetrofitApi {
-    @GET("https://api.github.com/search/repositories?q=language:java&page=1")
-   Call<ResponseBody> getRetrofit();
+    //get请求
+    @GET("https://api.github.com/{search}/repositories?q=language:java&page=1")
+    //@Headers({"zxc:123,123:123,xx:11"}) 添加消息头
+    Call<ResponseBody> getRetrofit(@Path("search")String search);
+
+    //post请求
+    @POST(" http://admin.syfeicuiedu.com/Handler/UserHandler.ashx?action=register")
+    Call<ResponseBody> postRetrofit(@Body RequestBody requestBody);
+
+    //表单请求
+    @FormUrlEncoded
+    @POST(" http://wx.feicuiedu.com:9094/yitao/UserWeb?method=register")
+    Call<ResponseBody> formRetrofit(@Field("username")String username,@Field("password")String password);
+
+    //多部分请求
+    @Multipart
+    @POST("http://wx.feicuiedu.com:9094/yitao/UserWeb?method=update")
+    Call<ResponseBody> multiRetrofit(@Part("username")RequestBody username);
 }
